5737dc607f34a54a38c73c9c2d86f95f59c2a5bf
Matthias Ringwald <matthias@ringwald.ch>
2022-11-02 15:22:49 +0100

    Use official USB VID/PID from pid.codes

--- firmware/pico/usb_descriptors.c
+++ firmware/pico/usb_descriptors.c
@@ -23,17 +23,8 @@
 #include "tusb.h"
 #include "usb_serial.h"
 
-/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
- * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
- *
- * Auto ProductID layout's Bitmap:
- *   [MSB]       MIDI | HID | MSC | CDC          [LSB]
- */
-#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-                           _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
-
-#define USB_VID   0xCafe
+#define USB_VID   0x1209
+#define USB_PID   0x2007
 #define USB_BCD   0x0200
 
 //--------------------------------------------------------------------+
