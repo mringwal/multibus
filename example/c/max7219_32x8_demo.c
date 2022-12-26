c4e8b4e3d0c9d6dbf22754256b11126cd7c9aab9
Matthias Ringwald <matthias@ringwald.ch>
2022-12-26 18:54:32 +0100

    example-c: simple text scroller

--- example/c/max7219_32x8_demo.c
+++ example/c/max7219_32x8_demo.c
@@ -29,7 +29,7 @@
 #include "multibus_serial_posix.h"
 #include "multibus_transport.h"
 #include "multibus_transport_protocol.h"
-
+#include "font8x8_basic.h"
 // This defines how many Max7219 modules we have cascaded together, in this case, we have 4 x 8x8 matrices giving a total of 32x8
 #define NUM_MODULES 4
 
@@ -40,6 +40,11 @@ const uint8_t CMD_BRIGHTNESS = 10;
 const uint8_t CMD_SCANLIMIT = 11;
 const uint8_t CMD_SHUTDOWN = 12;
 const uint8_t CMD_DISPLAYTEST = 15;
+
+// framebuffer
+#define DISPLAY_WIDTH (NUM_MODULES * 8)
+#define DISPLAY_HEIGHT 8
+static uint8_t framebuffer[NUM_MODULES * DISPLAY_HEIGHT];
 
 // static config
 static const char * multibus_bridge_path;
@@ -72,6 +77,41 @@ const mb_message_t * test_sync_wait_for_response(mb_transport_t * transport){
     return test_sync_message;
 }
 
+// minimal framebuffer
+
+static void fb_clear(void){
+    memset(framebuffer, 0, sizeof(framebuffer));
+}
+
+static void fb_set_pixel(uint16_t x, uint16_t y){
+    if (x < NUM_MODULES * 8) {
+        framebuffer[ (y*NUM_MODULES) + (x / 8)] |= 1 << ( 7 - (x & 7 ) );
+    }
+}
+
+static void fb_draw_character(int16_t x, int16_t y, uint8_t code){
+    if (code >= 128) return;
+    uint8_t i;
+    uint8_t j;
+    for (i=0;i<8;i++){
+        for (j=0;j<8;j++){
+            if (x + j < 0) continue;
+            if (font8x8_basic[code][i] & (1<<j)){
+                fb_set_pixel(x + j, y + i);
+            }
+        }
+    }
+}
+
+static void fb_draw_text(uint16_t offset, const char * text, uint16_t text_len){
+    uint16_t start_pos = offset / 8;
+    uint16_t i;
+    for (i=0;i<(NUM_MODULES+1);i++){
+        if (start_pos + i >= text_len) continue;
+        fb_draw_character((start_pos + i) * 8 - offset, 0, text[start_pos + i]);
+    }
+}
+
 static void max7219_write_register_all(mb_transport_t * transport, uint8_t address, uint8_t value){
     uint8_t buf[2 * NUM_MODULES];
     uint16_t i;
@@ -81,6 +121,22 @@ static void max7219_write_register_all(mb_transport_t * transport, uint8_t addre
     }
     mb_transport_spi_master_write_request_send(transport, 0, sizeof(buf), buf);
     (void) test_sync_wait_for_response(transport);
+}
+
+static void max7219_update_framebuffer(mb_transport_t * transport){
+    uint8_t buf[2 * NUM_MODULES];
+    uint16_t i;
+    uint16_t j;
+    for (i = 0; i<DISPLAY_HEIGHT ; i++){
+        for (j = 0; j<NUM_MODULES; j++) {
+            uint8_t value = framebuffer[(i*NUM_MODULES) + j];
+            buf[2*j]   = CMD_DIGIT0 + i;
+            buf[2*j+1] = framebuffer[(i*NUM_MODULES) + j];
+        }
+        printf("\n");
+        mb_transport_spi_master_write_request_send(transport, 0, sizeof(buf), buf);
+        (void) test_sync_wait_for_response(transport);
+    }
 }
 
 int main(int argc, const char **argv) {
@@ -123,8 +179,10 @@ int main(int argc, const char **argv) {
     max7219_write_register_all(transport, CMD_SHUTDOWN, 1);
     max7219_write_register_all(transport, CMD_BRIGHTNESS, 8);
 
+    uint16_t i;
+
     // simple vertical scroller
-    for (int i=0; i<64; i++) {
+    for (i=0; i<16; i++) {
         for (int j=0; j<8; j++) {
             max7219_write_register_all(transport, CMD_DIGIT0+j, 1 << (i&7));
         }
@@ -133,7 +191,7 @@ int main(int argc, const char **argv) {
     }
 
     // simple horizontal scroller
-    for (int i=0; i<64; i++) {
+    for (i=0; i<16; i++) {
         for (int j=0; j<8; j++) {
             max7219_write_register_all(transport, CMD_DIGIT0+j, ((i&7) == j) ? 255 : 0);
         }
@@ -143,8 +201,8 @@ int main(int argc, const char **argv) {
 
     // chess board blinky
     int bright = 1;
-    while (true) {
-        for (int i=0; i<8; i++) {
+    while (bright < 10) {
+        for (i=0; i<8; i++) {
             if (bright & 1) {
                 max7219_write_register_all(transport, CMD_DIGIT0 + i, 170 >> (i%2));
             } else {
@@ -157,6 +215,19 @@ int main(int argc, const char **argv) {
         (void) test_sync_wait_for_response(transport);
 
         bright++;
+    }
+
+    // text scroller
+    const char * text = "    Hello MultiBus!";
+    uint16_t text_len = strlen(text);
+    while (1){
+        for (i=0;i< (text_len * 8);i++){
+            fb_clear();
+            fb_draw_text(i, text, text_len);
+            max7219_update_framebuffer(transport);
+            mb_transport_bridge_delay_request_send(transport, 0, 100);
+            (void) test_sync_wait_for_response(transport);
+        }
     }
 
     // close down
